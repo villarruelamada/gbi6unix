49c089da3047b5879c61f831d0ad7baf522b8336
villarruelamada <117691160+villarruelamada@users.noreply.github.com>
2023-04-27 19:05:32 -0500

    Cambios realizados

--- contador.sh
+++ contador.sh
@@ -1,4 +1,4 @@
 #!/bin/bash/
-head -n 2 Pacifici2013_data.csv | cut -d ";" -f 2-6 | tr ";" " " > animales.csv 
-
+head -n 20 Pacifici2013_data.csv | cut -d " " -f 2-6 | tr ";" " " > animales.csv 
+cat animales.csv | cut -d " " -f 1 | sort | uniq -c 
 
